033e525359ca88afd09d3adbe68f083d49138e4b
Ryan Parrish <rino3717@gmail.com>
2020-03-24 16:39:26 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,12 @@
 #write your code here
 
-def countdown
+def countdown (x)
+x = 10
+while x > 0
+  puts "#{x} SECOND(S)!"
+  x -= 1
 end
+end
+"HAPPY NEW YEAR!"
+
+
---
+++ countdownwithsleep.rb
@@ -0,0 +1,9 @@
+def countdown_with_sleep(sleep=1)
+  sleep
+  x = 10
+  while x > 0
+    puts "#{x} SECOND(S)!"
+    x -= 1
+  end
+  "HAPPY NEW YEAR!"
+end
